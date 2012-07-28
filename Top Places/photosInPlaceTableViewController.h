3db3183bc43d9bdbbb45ad1b7b9062f21e077ff6
Maxim Piskunov <maxitg@neutrino.local>
2012-07-28 17:25:01 +0400

    added photosInPlaceTableViewController

---
+++ Top Places/photosInPlaceTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaceTopPhotosTableViewController.h
+//  Top Places
+//
+//  Created by Maxim Piskunov on 28.07.2012.
+//  Copyright (c) 2012 Maxim Piskunov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface photosInPlaceTableViewController : UITableViewController
+
+@end
