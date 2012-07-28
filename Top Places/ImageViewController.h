cd6d9aa19875c7b6fa11fab5a0ee0a1b4cede5b9
Maxim Piskunov <maxitg@neutrino.local>
2012-07-28 18:07:00 +0400

    implemented image view controller

---
+++ Top Places/ImageViewController.h
@@ -0,0 +1,16 @@
+//
+//  ImageViewController.h
+//  Top Places
+//
+//  Created by Maxim Piskunov on 28.07.2012.
+//  Copyright (c) 2012 Maxim Piskunov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@property (nonatomic, strong) NSURL *imageURL;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
---
+++ Top Places/ImageViewController.m
@@ -0,0 +1,55 @@
+//
+//  ImageViewController.m
+//  Top Places
+//
+//  Created by Maxim Piskunov on 28.07.2012.
+//  Copyright (c) 2012 Maxim Piskunov. All rights reserved.
+//
+
+#import "ImageViewController.h"
+
+@interface ImageViewController ()
+
+@end
+
+@implementation ImageViewController
+
+@synthesize imageURL = _imageURL;
+@synthesize imageView = _imageView;
+
+- (void)setImageURL:(NSURL *)imageURL
+{
+    if (imageURL != _imageURL) {
+        _imageURL = imageURL;
+        self.imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:imageURL]];
+    }
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [self setImageView:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
