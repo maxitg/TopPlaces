955dbaaba24c6bbfd682aebb86270d1f46428a84
Maxim Piskunov <maxitg@neutrino.local>
2012-07-30 11:25:54 +0400

    cleaned up topPlacesTVC

--- Top Places/TopPlacesTableViewController.m
+++ Top Places/TopPlacesTableViewController.m
@@ -12,13 +12,15 @@
 
 @interface TopPlacesTableViewController ()
 
-@property (nonatomic, strong) NSArray* topPlaces;
+@property (nonatomic, strong) NSArray* topPlaces;   //  it is a model
 
 @end
 
 @implementation TopPlacesTableViewController
 
 @synthesize topPlaces = _topPlaces;
+
+#pragma mark - Setters & getters
 
 - (NSArray*)topPlaces
 {
@@ -31,20 +33,7 @@
     return _topPlaces;
 }
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    self.title = @"Top Places";
-}
+#pragma mark - Lifecycle
 
 - (void)viewDidUnload
 {
@@ -57,6 +46,8 @@
 {
     return ([[UIDevice currentDevice] userInterfaceIdiom] != UIUserInterfaceIdiomPhone || interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
+
+#pragma mark - Segues
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
@@ -77,8 +68,8 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Place Description"];
-    
-    NSString *placeDescription = [[self.topPlaces objectAtIndex:indexPath.row] valueForKey:@"_content"];
+        
+    NSString *placeDescription = [[self.topPlaces objectAtIndex:indexPath.row] valueForKey:FLICKR_PLACE_NAME];
     NSArray *placeComponents = [placeDescription componentsSeparatedByString:@", "];
     cell.textLabel.text = [placeComponents objectAtIndex:0];
     
@@ -89,58 +80,6 @@
     cell.detailTextLabel.text = [placeDetailes componentsJoinedByString:@", "];
     
     return cell;
-}
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-#pragma mark - Table view delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
 }
 
 @end
