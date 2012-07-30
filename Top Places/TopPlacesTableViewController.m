e0a105c89ef264b010b10782ed1af7adb48ac664
Maxim Piskunov <maxitg@neutrino.local>
2012-07-30 18:50:06 +0400

    added sections to top places

--- Top Places/TopPlacesTableViewController.m
+++ Top Places/TopPlacesTableViewController.m
@@ -12,7 +12,7 @@
 
 @interface TopPlacesTableViewController ()
 
-@property (nonatomic, strong) NSArray* topPlaces;
+@property (nonatomic, strong) NSArray* topPlaces;   //  of (arrays of pairs {location components, place description}), sorted by country
 
 @end
 
@@ -25,9 +25,32 @@
 - (NSArray*)topPlaces
 {
     if (!_topPlaces) {
-        NSArray *unsortedPlaces = [FlickrFetcher topPlaces];
-        _topPlaces = [unsortedPlaces sortedArrayUsingComparator: ^(id obj1, id obj2) {
-            return [[obj1 objectForKey:@"_content"] compare:[obj2 objectForKey:@"_content"] options:NSCaseInsensitiveSearch];
+        NSArray *rawPlaces = [FlickrFetcher topPlaces];
+        
+        //  building NSMutableDictionary of arrays of pairs {location components, place description}
+        
+        NSMutableDictionary *countries = [[NSMutableDictionary alloc] init];
+        for (NSDictionary *placeDescription in rawPlaces) {
+            NSString *name = [placeDescription objectForKey:FLICKR_PLACE_NAME];
+            NSArray *nameComponents = [name componentsSeparatedByString:@", "];
+            NSString *countryName = [nameComponents lastObject];
+            if (![countries objectForKey:countryName]) [countries setObject:[[NSMutableArray alloc] init] forKey:countryName];
+            [[countries objectForKey:countryName] addObject:[NSArray arrayWithObjects:nameComponents, placeDescription, nil]];
+        }
+        
+        //  building NSMutableArray of (arrays of pairs {location components, place description}, sorted by place name)
+        
+        NSMutableArray *topPlacesMutable = [[NSMutableArray alloc] init];
+        for (NSString *countryName in [countries allKeys]) {
+            [topPlacesMutable addObject:[[countries objectForKey:countryName] sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+                return [[obj1 componentsJoinedByString:@", "] compare:[obj2 componentsJoinedByString:@", "]];
+            }]];
+        }
+        
+        //  building NSArray of (arrays of pairs {location components, place description}, sorted by place name), sorted by country
+        
+        _topPlaces = [topPlacesMutable sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+            return [[[[obj1 firstObject] firstObject] lastObject] compare:[[[obj2 firstObject] firstObject] lastObject]];
         }];
     }
     return _topPlaces;
@@ -52,7 +75,8 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([segue.identifier isEqualToString:@"Show Photos In Place"]) {
-        NSDictionary *chosenPlace = [self.topPlaces objectAtIndex:[self.tableView indexPathForCell:sender].row];
+        NSIndexPath *senderIndexPath = [self.tableView indexPathForCell:sender];
+        NSDictionary *chosenPlace = [[[self.topPlaces objectAtIndex:senderIndexPath.section] objectAtIndex:senderIndexPath.row] lastObject];
         [segue.destinationViewController setPhotos:[FlickrFetcher photosInPlace:chosenPlace maxResults:50]];
         [segue.destinationViewController setTitle:[[sender textLabel] text]];
     }
@@ -60,17 +84,26 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return [self.topPlaces count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[self.topPlaces objectAtIndex:section] count];
+}
+
+- (NSString*)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return [[[[self.topPlaces objectAtIndex:section] firstObject] firstObject] lastObject];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Place Description"];
-        
-    NSString *placeDescription = [[self.topPlaces objectAtIndex:indexPath.row] valueForKey:FLICKR_PLACE_NAME];
-    NSArray *placeComponents = [placeDescription componentsSeparatedByString:@", "];
+    
+    NSArray *placeComponents = [[[self.topPlaces objectAtIndex:indexPath.section] objectAtIndex:indexPath.row] firstObject];
     cell.textLabel.text = [placeComponents objectAtIndex:0];
     
     NSRange placeDetailesRange;
